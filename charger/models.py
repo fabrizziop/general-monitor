e4149d7dae63ae969e19166d634f57fd7314f9aa
Fabrizzio <fabrizziopm@gmail.com>
2019-09-30 22:13:54 -0400

    some models and tests

--- charger/models.py
+++ charger/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class ChargerModel(models.Model):
+	identifier_key = models.CharField(max_length=64, unique=True)
+
+class IndividualMeasurementModel(models.Model):
+	specific_charger = models.ForeignKey(ChargerModel, on_delete=models.CASCADE)
+	timestamp = models.DateTimeField(default=timezone.now)
--- charger/tests.py
+++ charger/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
-
+from django.core.exceptions import ValidationError
+from .models import ChargerModel, IndividualMeasurementModel
 # Create your tests here.
+
+class APITests(TestCase):
+	def test_POSTing_instant_measurement(self):
+		response = self.client.post(
+			'/new-measurement',
+			{
+			'current': 10
+			}
+			)
+
+class ModelTests(TestCase):
+
+	def test_charger_model_uniqueness(self):
+		a = ChargerModel(identifier_key = "a" * 64)
+		a.full_clean()
+		a.save()
+		with self.assertRaises(ValidationError):
+			b = ChargerModel(identifier_key = "a" * 64)
+			b.full_clean()
+			b.save()
+
+	def test_indiv_meas_model_foreign_key(self):
+		a = ChargerModel(identifier_key = "a" * 64)
+		a.full_clean()
+		a.save()
+		b = IndividualMeasurementModel(specific_charger=a)
+		b.full_clean()
+		b.save()
+		c = IndividualMeasurementModel(specific_charger=a)
+		c.full_clean()
+		c.save()
+		self.assertIn(b, list(a.individualmeasurementmodel_set.all()))
+		self.assertIn(c, list(a.individualmeasurementmodel_set.all()))
