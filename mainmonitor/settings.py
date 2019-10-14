fad46309f6042284518d6462b3cd2c0c878a3287
Fabrizzio <fabrizziopm@gmail.com>
2019-10-14 12:33:48 -0400

    static root

--- mainmonitor/settings.py
+++ mainmonitor/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #MEDIA_URL = '/media/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
