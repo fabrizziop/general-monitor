f44c5181d954c7506044d56755efc21ac793f1cf
Fabrizzio <fabrizziopm@gmail.com>
2019-10-18 10:20:55 -0400

    using env

--- mainmonitor/settings.py
+++ mainmonitor/settings.py
@@ -25,13 +25,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6ws!^!x2ku+=#mva-g2$3cs%z8dz@r93i!=ltqqe!g%ca%cvyh'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+if 'DJANGO_DEBUG_FALSE' in os.environ:  
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']  
+    ALLOWED_HOSTS = [os.environ['SITENAME'], 'localhost']  
+else:
+    DEBUG = True  
+    SECRET_KEY = '6ws!^!x2ku+=#mva-g2$3cs%z8dz@r93i!=ltqqe!g%ca%cvyh'
+    ALLOWED_HOSTS = []
 
 
 # Application definition
